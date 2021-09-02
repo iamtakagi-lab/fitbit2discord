2f7b1b05bd4c54aadb9a2f478e5f08ded6c96701
takagi <iam.takagi.dev@gmail.com>
2021-09-03 00:20:45 +0900

    Add 階数の単位、階を追記

--- src/notifier.ts
+++ src/notifier.ts
@@ -8,10 +8,10 @@ const makeEmbed = ({ heartrate, miles, calories, floors, sleep }: Cache) => {
             .setTitle(`${env.NAME} - 人体`)
             .setDescription("現在のアクティビティを表示しています")
             .setThumbnail("https://irishtechnews.ie/wp-content/uploads/2016/08/fitbit-logo.png")
-            .addField(":heart: 心拍数", heartrate + "bpm", true)
+            .addField(":heart: 心拍数", `${heartrate} bpm`, true)
             .addField(":fire: 消費済みカロリー", calories.toString(), true)
-            .addField(":athletic_shoe: 歩いた距離", miles + "mi", true)
-            .addField(":triangular_ruler: 歩いた階数", floors.toString(), true)
+            .addField(":athletic_shoe: 歩いた距離", `${miles} mi`, true)
+            .addField(":triangular_ruler: 歩いた階数", `${floors.toString()} 階`, true)
             .addField(":zzz: 睡眠時間", sleep, true)
     )
 }
