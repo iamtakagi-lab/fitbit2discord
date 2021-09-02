c1074660e35d780718cdcc2d5be26cfbf0faf179
takagi <iam.takagi.dev@gmail.com>
2021-09-02 23:39:45 +0900

    Support koa with https

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,24 @@ import Credential from './credential';
 import Fitbit from './fitbit';
 import authorize from './routes/authorize';
 import callback from './routes/callback';
+import fs from 'fs'
+import https from 'https'
+import env from './env';
+
+var key = fs.readFileSync(__dirname + '/certs/server.key');
+var cert = fs.readFileSync(__dirname + '/certs/server.crt');
+var options = {
+    key: key,
+    cert: cert
+};
 
 export default (credential: Credential, fitbit: Fitbit) => {
     const router = new Router().all('/authorize', authorize(credential, fitbit)).all('/callback', callback(credential, fitbit))
-    new Koa()
-        .use(router.allowedMethods())
-        .use(router.routes())
-        .listen(Number(process.env.PORT), () => {
-        console.log("Fitbit authorization server started on : " + process.env.PORT);
-    });
+    const koa = new Koa()
+            .use(router.allowedMethods())
+            .use(router.routes())
+
+    https.createServer(options, koa.callback()).listen(env.PORT, () => {
+        console.log("Fitbit authorization server started on : " + env.PORT);
+    })
 }
