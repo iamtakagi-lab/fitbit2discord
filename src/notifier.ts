1713beebb2c3111a224c66afbb7dfa65f3642403
takagi <iam.takagi.dev@gmail.com>
2021-09-06 00:21:08 +0900

    Add color for embed

--- src/notifier.ts
+++ src/notifier.ts
@@ -5,28 +5,29 @@ import env from './env'
 
 const makeEmbed = ({ heartrate, miles, calories, floors, sleep }: Cache) => {
     const embed = new MessageEmbed()
-            .setTitle(`${env.NAME} - 人体`)
-            .setDescription("現在のアクティビティを表示しています")
-            .setThumbnail(FITBIT_THUMBNAIL_URL)
-            .addField(":heart: 心拍数", heartrate, true)
-            .addField(":fire: 消費済みカロリー", calories, true)
-            .addField(":athletic_shoe: 歩いた距離", miles, true)
-            .addField(":triangular_ruler: 歩いた階数", floors, true)
-            .addField(":zzz: 睡眠時間", sleep, true)
+        .setColor('#F5E81F')
+        .setTitle(`${env.NAME} - 人体`)
+        .setDescription("現在のアクティビティを表示しています")
+        .setThumbnail(FITBIT_THUMBNAIL_URL)
+        .addField(":heart: 心拍数", heartrate, true)
+        .addField(":fire: 消費済みカロリー", calories, true)
+        .addField(":athletic_shoe: 歩いた距離", miles, true)
+        .addField(":triangular_ruler: 歩いた階数", floors, true)
+        .addField(":zzz: 睡眠時間", sleep, true)
 
     if (env.WEBSITE_URL != null && env.WEBSITE_URL.length > 0) {
         embed.setURL(`${env.WEBSITE_URL}`)
     }
 
     return Promise.resolve(
-        embed 
+        embed
     )
 }
 
-const notify = async ({id, token}: any, cache: Cache) => {
+const notify = async ({ id, token }: any, cache: Cache) => {
     makeEmbed(cache).then(async (embed) => {
-        await new WebhookClient({id, token}).send({
-			embeds: [embed],
+        await new WebhookClient({ id, token }).send({
+            embeds: [embed],
         })
     })
 }
